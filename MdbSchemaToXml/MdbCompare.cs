14513f6889b92917863aa7d85bcf160b146cc93c
Joe Reynolds <joseph@clear-measure.com>
2014-07-28 23:04:31 -0500

    initial commit of main source

---
+++ MdbSchemaToXml/MdbCompare.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Xml.Linq;
+
+namespace MdbSchemaToXml
+{
+    class MdbToXml
+    {
+        public string PathToData { get; set; }
+        public string PathToOutput { get; set; }
+
+        public MdbToXml(string pathToData, string pathToOutput)
+        {
+            PathToData = pathToData;
+            PathToOutput = pathToOutput;
+            GenerateXmlOutput();
+        }
+
+        void GenerateXmlOutput()
+        {
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + PathToData;
+            var databaseConnection = new OleDbConnection(connectionString);
+            databaseConnection.Open();
+
+            var databaseElement = new XElement("Root");
+
+            DataTable dataTable = databaseConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            if (dataTable != null)
+            {
+
+                int numTables = dataTable.Rows.Count;
+                for (int tableIndex = 0; tableIndex < numTables; ++tableIndex)
+                {
+                    var tableName = dataTable.Rows[tableIndex]["TABLE_NAME"].ToString();
+                    var dataTableElement = new XElement("Table", tableName);
+                    databaseElement.Add(dataTableElement);
+
+
+                    var schemaTable = databaseConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Columns, new object[] { null, null, tableName, null });
+                    if (schemaTable != null)
+                        foreach (DataRow row in schemaTable.Rows)
+                        {
+                            var fieldName = row[3];
+                            var fieldType = row[11];
+                            var fieldDescription = row[27];
+                            var fieldElement = new XElement("Field", fieldName);
+                            dataTableElement.Add(fieldElement);
+                        }
+                }
+            }
+            Console.WriteLine(databaseElement);
+            databaseElement.Save(PathToOutput);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MdbSchemaToXml/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using CODE.Framework.Core.Utilities;
+
+namespace MdbSchemaCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCompare = new MdbCompare(@"C:\Temp\20140728\new.mdb", @"C:\Temp\20140728\new.xml");
+            var oldCompare = new MdbCompare(@"C:\Temp\20140728\old.mdb", @"C:\Temp\20140728\old.xml");
+        }
+    }
+}
